e0d7c542dc9296356aa31c20bc2e5eb6fbb89442
Asamdpd <danieldimitrov.az@gmail.com>
2021-08-19 20:00:13 +0300

    Project Completed
    Completed all the tasks as well as the optional task. UI and functionality working as intended

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const size = document.querySelector('input[id="size"]');
 const pointsLabel = document.querySelector('label[id="pointsLabel"]');
 const container = document.querySelector('div[class="container"]');
-const color = document.querySelector('input[id="color"]')
+const color = document.querySelector('input[id="color"]');
+const clearBtn = document.getElementById("clearBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
 
+let cont = false;
 let divs;
-let trigger = false;
+let trigger = true;
+let rainbowTrigger = false;
+let o = Math.round, r = Math.random, s = 255;
 
-
-pointsLabel.innerHTML = `${size.value} x ${size.value}`
-console.log(size.value)
-
+pointsLabel.innerHTML = `${size.value} x ${size.value}`;
 size.addEventListener("input", () => {
     pointsLabel.innerHTML = `${size.value} x ${size.value}`;
     
@@ -17,74 +19,81 @@ size.addEventListener("input", () => {
 })
 
 size.addEventListener("change", () => {
-    container.style.gridTemplateColumns = `repeat(${size.value}, 1fr)`
-    test()
+    container.style.gridTemplateColumns = `repeat(${size.value}, 1fr)`;
+    createDivs();
 })
 
 const div = document.createElement("div");
 
 
-function test() {
-    
-    let elem;
-    
+function createDivs() {
 
     document.querySelectorAll('.square').forEach(e => e.remove());
     for(i = 0; i < size.value; i++){
         for(y = 0; y < size.value; y++){
-           container.appendChild(document.createElement("div")) ;
+           container.appendChild(document.createElement("div"));
         }
-    }   
+    }     
     
-    
-     divs = container.querySelectorAll(":scope > div")
+     divs = container.querySelectorAll(":scope > div");
      for(i=0; i < divs.length; i++){
 
         
-         divs[i].classList.add("square")
-         divs[i].setAttribute("id", `${i}`)
+         divs[i].classList.add("square");
+         divs[i].setAttribute("id", `${i}`);
          
-         elemen= document.querySelector(`div[id="${i}"]`)
-         console.log(elem)
-         
+         elemen= document.querySelector(`div[id="${i}"]`);       
      }
+     eventListeners();
+}
 
-     divs.forEach(div => {
-         div.addEventListener("click", () => {
-            div.style.backgroundColor = `${color.value}`;
-            trigger = !trigger
-            divs.forEach(div => {
-                         div.addEventListener("mouseover", () => {
-                             if(trigger === true){
-                                 div.style.backgroundColor = `${color.value}`;
-                             }
-                            
-                         })
-                        
-                     })
-     })
+function eventListeners(){
+    divs.forEach(div => {
+        div.addEventListener("click", () => {
+            cont = !cont;
+            if(rainbowTrigger){
+               div.style.backgroundColor = 'rgb(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s);   
+            }else{
+           div.style.backgroundColor = `${color.value}`;}
+           divs.forEach(div => {
+                        div.addEventListener("mouseover", () => {
+                            if(cont){
+                               if(trigger === true){
+                                   div.style.backgroundColor = `${color.value}`;
+                               }
+                               if(rainbowTrigger){
+                                  
+                                   trigger = false;
+                                   div.style.backgroundColor = 'rgb(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s); 
+                               }
+                            }                         
+                        })
+                       
+                    })
+        })
+    })
+}
+
+clearBtn.addEventListener("click", () => {
+    divs.forEach(div => div.style.backgroundColor = "rgb(255, 255, 255)");
+})
+
+rainbowBtn.addEventListener("click", () => {
+        rainbowTrigger = !rainbowTrigger;
+        trigger= !trigger;
+        if(rainbowTrigger){
+            rainbowBtn.style.backgroundColor = "#5555f8";
+        }else{
+            rainbowBtn.style.backgroundColor =  "#8080ff";
+        }
+        
     })
 
-    //  container.addEventListener("click", () => {
-         
-    //     trigger = !trigger
-    //     divs.forEach(div => {
-    //         div.addEventListener("mouseover", () => {
-    //             if(trigger === true){
-    //                 div.style.backgroundColor = `${color.value}`;
-    //             }
-                
-    //         })
-            
-    //     })
-    //  })
 
-
- 
+color.addEventListener("click", () => {
+    trigger = true;
     
-     
-     
-    console.log(divs)
+    
+})
 
-}
-test()
+createDivs();
